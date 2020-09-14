4ae9844b95243a2ed3ab5122dd26c4bb17828419
christian <ChristiaNotarmaso@gmail.com>
2020-09-14 15:50:23 +0200

    Første commit

---
+++ app/src/main/java/com/example/s195465/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.s195465;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bVej;
+    Button bInd;
+    TextView tStatus;
+    EditText urlEdit;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bVej = findViewById(R.id.buttonWeb);
+        bInd = findViewById(R.id.buttonInd);
+        tStatus = findViewById(R.id.textViewStatus);
+        urlEdit = findViewById(R.id.inputURL);
+        webView = findViewById(R.id.webView1);
+        final ImageView picture = findViewById(R.id.imageViewMain);
+        webView.setVisibility(View.GONE);
+
+        bVej.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tStatus.setText("WebView");
+                picture.setImageResource(0);
+                webView.setVisibility(View.VISIBLE);
+                webView.setWebViewClient(new WebViewClient());
+                webView.loadUrl(urlEdit.getText().toString());
+
+            }
+        });
+        bInd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webView.setVisibility(View.GONE);
+                tStatus.setText("Menu");
+                picture.setImageResource(R.drawable.xv17fnu);
+            }
+        });
+
+    }
+}
